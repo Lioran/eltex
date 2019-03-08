ff7d69e36766a4170697c634922de4e1d90d23af
Lioran <wersil@mail.ru>
2019-03-08 22:54:01 +0700

    Исправлена ошибка приема данных, но не ошибка доступа к полю

--- Hibernate/src/main/java/com/ex/Rest.java
+++ Hibernate/src/main/java/com/ex/Rest.java
@@ -1,13 +1,15 @@
 package com.ex;
 
 
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.io.IOException;
 /**
  * Класс Rest-контроллера
  * @author Dmitrii
@@ -34,21 +36,25 @@ class Rest{
         return (ArrayList<Abonent>)abonentInPhoneBook.getAllAbonent();
     }
 
-    /*@RequestMapping("/addAbonent")
-    public void getAbonent(HashMap<String, String> WhatCame){
-        AbonentInPhoneBook abonentInPhoneBook = new AbonentInPhoneBook();
-        Abonent somebodyAbonent = new Abonent();
-        somebodyAbonent.setFio(WhatCame.get("fio"));
-        somebodyAbonent.setPhone(WhatCame.get("phone"));
-        somebodyAbonent.setAdress(WhatCame.get("adress"));
-        System.out.println(WhatCame.size()+" "+WhatCame.get("fio")+" "+WhatCame.get("phone")+" "+WhatCame.get("adress"));
-        abonentInPhoneBook.save(somebodyAbonent);
-    }*/
     @RequestMapping(value = "/addAbonent", method = RequestMethod.POST)
-    public void getAbonent(String WhatCame){
-        AbonentInPhoneBook abonentInPhoneBook = new AbonentInPhoneBook();
-        Abonent somebodyAbonent = new Abonent();
-        System.out.println(WhatCame);
-        //abonentInPhoneBook.save(somebodyAbonent);
+    public void getAbonent(@RequestBody String inputStr){
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            HashMap<String, String> jsonStr;
+            jsonStr = mapper.readValue(inputStr, new TypeReference<HashMap<String, String>>(){});
+            AbonentInPhoneBook abonentInPhoneBook = new AbonentInPhoneBook();
+            Abonent somebodyAbonent = new Abonent();
+            somebodyAbonent.setFio(jsonStr.get("fio"));
+            somebodyAbonent.setPhone(jsonStr.get("phone"));
+            somebodyAbonent.setAdress(jsonStr.get("adress"));
+            abonentInPhoneBook.save(somebodyAbonent);
+        }
+        catch (JsonGenerationException e){
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
