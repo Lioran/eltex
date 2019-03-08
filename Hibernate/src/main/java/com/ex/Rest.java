bcf54b5ed9a07da061c56446a553afb2a1dc32fd
Lioran <wersil@mail.ru>
2019-03-08 20:29:10 +0700

    Неразрешенная проблема с web-частью

--- Hibernate/src/main/java/com/ex/Rest.java
+++ Hibernate/src/main/java/com/ex/Rest.java
@@ -1,11 +1,12 @@
 package com.ex;
 
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Класс Rest-контроллера
@@ -33,8 +34,8 @@ class Rest{
         return (ArrayList<Abonent>)abonentInPhoneBook.getAllAbonent();
     }
 
-    @RequestMapping("/addAbonent")
-    public void getAbonent(Map<String, String>WhatCame){
+    /*@RequestMapping("/addAbonent")
+    public void getAbonent(HashMap<String, String> WhatCame){
         AbonentInPhoneBook abonentInPhoneBook = new AbonentInPhoneBook();
         Abonent somebodyAbonent = new Abonent();
         somebodyAbonent.setFio(WhatCame.get("fio"));
@@ -42,5 +43,12 @@ class Rest{
         somebodyAbonent.setAdress(WhatCame.get("adress"));
         System.out.println(WhatCame.size()+" "+WhatCame.get("fio")+" "+WhatCame.get("phone")+" "+WhatCame.get("adress"));
         abonentInPhoneBook.save(somebodyAbonent);
+    }*/
+    @RequestMapping(value = "/addAbonent", method = RequestMethod.POST)
+    public void getAbonent(String WhatCame){
+        AbonentInPhoneBook abonentInPhoneBook = new AbonentInPhoneBook();
+        Abonent somebodyAbonent = new Abonent();
+        System.out.println(WhatCame);
+        //abonentInPhoneBook.save(somebodyAbonent);
     }
 }
